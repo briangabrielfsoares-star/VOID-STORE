a7c06797d01b51d146ddd06f52b09c5272021db0
briangabrielfsoares-star <briangabrielfsoares@gmail.com>
2026-06-25 16:55:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ console.log("VOID carregada com sucesso.");
 
 document.addEventListener("DOMContentLoaded", () => {
 
+let carrinho = JSON.parse(localStorage.getItem("voidCarrinho")) || [];
+
+let favoritos = JSON.parse(localStorage.getItem("voidFavoritos")) || [];
+
+/* ANIMAÇÃO DOS CARDS */
+
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
@@ -12,6 +18,68 @@ card.style.transform = "translateY(-8px)";
 
 card.addEventListener("mouseleave", () => {
 card.style.transform = "translateY(0px)";
+});
+
+});
+
+/* ADICIONAR AO CARRINHO */
+
+const botoesCarrinho = document.querySelectorAll(".add-cart");
+
+botoesCarrinho.forEach(botao => {
+
+botao.addEventListener("click", () => {
+
+const produto = {
+
+nome: botao.dataset.nome,
+
+preco: botao.dataset.preco,
+
+imagem: botao.dataset.imagem
+
+};
+
+carrinho.push(produto);
+
+localStorage.setItem(
+"voidCarrinho",
+JSON.stringify(carrinho)
+);
+
+alert("Produto adicionado ao carrinho!");
+
+});
+
+});
+
+/* FAVORITOS */
+
+const botoesFavoritos = document.querySelectorAll(".add-favorite");
+
+botoesFavoritos.forEach(botao => {
+
+botao.addEventListener("click", () => {
+
+const produto = {
+
+nome: botao.dataset.nome,
+
+preco: botao.dataset.preco,
+
+imagem: botao.dataset.imagem
+
+};
+
+favoritos.push(produto);
+
+localStorage.setItem(
+"voidFavoritos",
+JSON.stringify(favoritos)
+);
+
+alert("Adicionado aos favoritos!");
+
 });
 
 });
